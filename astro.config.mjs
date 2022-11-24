4929048d1fa78693365b0d516b7e52aa9f9aa5b1
李瑞丰🤡 <lirf@lenovocloud.com>
2022-11-24 15:15:20 +0800

    feat: astro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://liruifengv.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(),     
+    image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  }),],
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
